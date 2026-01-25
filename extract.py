ce2abddbe46666f03173a62267a5d924e3bbfa6c
Andrei Carvalho <andreicarvs@gmail.com>
2026-01-25 20:10:53 -0300

    feat: change API endpoint to get all coins market and add path validation

--- extract.py
+++ extract.py
@@ -1,9 +1,11 @@
 import requests
 from requests.adapters import HTTPAdapter, Retry
+from itertools import batched
 import json
 import os
 
 def get_api_data(url, headers, file_name, total_retries : int = 5):
+   file_path = os.path.join('data/raw', file_name)
    s = requests.Session()
 
    retries = Retry(total=total_retries, 
@@ -16,37 +18,32 @@ def get_api_data(url, headers, file_name, total_retries : int = 5):
       
       response.raise_for_status()
 
-      with open(file=f'{file_name}.json', mode='w') as file:
+      with open(file=f'{file_path}.json', mode='w') as file:
          json.dump(response.json(), fp=file, indent=4)
       
    except Exception as e:
       print(f'Error while getting API data: {e}')
       raise
 
-def get_coins_ids(headers):
-   url = 'https://api.coingecko.com/api/v3/coins/list'
+# def get_coins_ids(headers):
+#    url = 'https://api.coingecko.com/api/v3/coins/list'
 
-   get_api_data(url, headers, 'coins_ids')
+#    get_api_data(url, headers, 'coins_ids')
 
-# def get_coins_price():
-#    with open('coins_ids.json') as file:
-#       coins = json.load(fp=file)
+# def get_coins_market_cap(headers):
+#    url = 'https://api.coingecko.com/api/v3/global'
 
-   # for coin in coins:
-   #    coin_id = coin['id']
+#    get_api_data(url, headers, 'coins_market_cap')
 
-   # url = 'https://api.coingecko.com/api/v3/coins/list'
-   # get_api_data(url, headers, 'coins_price')
+def get_coins_market(headers):
+      
+   url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
+
+   get_api_data(url, headers, 'coins_market')
 
 
 if __name__ == '__main__':
+   os.makedirs('data/raw', exist_ok=True)
    api_key = os.getenv('API_KEY')
    headers = {'x-cg-demo-api-key' : api_key}
-   get_coins_ids(headers)
-
-   # get_coins_price()
-
-
-
-
-# response = requests.get('https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd', headers=headers)
+   get_coins_market(headers)
