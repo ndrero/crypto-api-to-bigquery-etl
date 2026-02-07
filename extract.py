cd429bb16a06bdb7795fee46e2dc2a950fe6b59c
Andrei Carvalho <andreicarvs@gmail.com>
2026-02-07 14:04:14 -0300

    style: fix indentation in python files

--- extract.py
+++ extract.py
@@ -8,77 +8,86 @@ import os
 
 logger = get_logger(__name__)
 
-def get_api_data(url, headers, file_name, local_dir, total_retries : int = 5):
-   file_path = os.path.join(local_dir, file_name)
-   s = requests.Session()
 
-   retries = Retry(total=total_retries, 
-                   backoff_factor=1, 
-                   status_forcelist=[408, 425, 429, 500, 502, 503, 504])
-   s.mount('https://', HTTPAdapter(max_retries=retries))
+def get_api_data(url, headers, file_name, local_dir, total_retries: int = 5):
+    file_path = os.path.join(local_dir, file_name)
+    s = requests.Session()
 
-   try:
-      logger.info(f'Trying to get API data from {url}')
-      response = s.get(url, headers=headers, timeout=30)
-      
-      response.raise_for_status()
+    retries = Retry(
+        total=total_retries,
+        backoff_factor=1,
+        status_forcelist=[408, 425, 429, 500, 502, 503, 504],
+    )
+    s.mount("https://", HTTPAdapter(max_retries=retries))
 
-      logger.info('Sucessfully fetched API data')
-      with open(file=f'{file_path}.json', mode='w') as file:
-         logger.info(f'Loading data into {file_path}')
-         json.dump(response.json(), fp=file, indent=4)
-      
-   except requests.exceptions.HTTPError as e:
-      logger.error(f'HTTP error (Status {response.status_code}) while accessing {url}: {e}')
-      raise
+    try:
+        logger.info(f"Trying to get API data from {url}")
+        response = s.get(url, headers=headers, timeout=30)
 
-   except requests.exceptions.ConnectionError as e:
-      logger.error(f'Connection error: Couldn\'t reach API in {url}: {e}')
-      raise
+        response.raise_for_status()
 
-   except Exception as e:
-      logger.error(f'API unexpected error : {e}')
-      raise
+        logger.info("Sucessfully fetched API data")
+        with open(file=f"{file_path}.json", mode="w") as file:
+            logger.info(f"Loading data into {file_path}")
+            json.dump(response.json(), fp=file, indent=4)
+
+    except requests.exceptions.HTTPError as e:
+        logger.error(
+            f"HTTP error (Status {response.status_code}) while accessing {url}: {e}"
+        )
+        raise
+
+    except requests.exceptions.ConnectionError as e:
+        logger.error(f"Connection error: Couldn't reach API in {url}: {e}")
+        raise
+
+    except Exception as e:
+        logger.error(f"API unexpected error : {e}")
+        raise
+
 
 def get_coins_market(headers, local_dir):
-      
-   url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
 
-   get_api_data(url, headers, 'coins_market', local_dir)
+    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
+
+    get_api_data(url, headers, "coins_market", local_dir)
+
 
 def load_raw_data(local_dir):
-   bucket = get_bucket('crypto-prj-bucket')
+    bucket = get_bucket("crypto-prj-bucket")
 
-   for file in os.listdir(local_dir):
-      logger.info(f'Starting {file} load into bronze storage bucket')
-      local_file_path = os.path.join(local_dir, file)
-      load_file_path = f'bronze/{date.today()}/{file}'
-      blob = bucket.blob(load_file_path)
+    for file in os.listdir(local_dir):
+        logger.info(f"Starting {file} load into bronze storage bucket")
+        local_file_path = os.path.join(local_dir, file)
+        load_file_path = f"bronze/{date.today()}/{file}"
+        blob = bucket.blob(load_file_path)
 
-      try:
-         if blob.exists():
-            logger.info(f'Deleting previous bronze storage blob')
-            blob.delete()
+        try:
+            if blob.exists():
+                logger.info(f"Deleting previous bronze storage blob")
+                blob.delete()
 
-         logger.info(f'Loading blob into bucket')
-         blob.upload_from_filename(local_file_path)
+            logger.info(f"Loading blob into bucket")
+            blob.upload_from_filename(local_file_path)
 
-      except FileNotFoundError:
-         logger.error(f'File not found at {local_file_path}')
+        except FileNotFoundError:
+            logger.error(f"File not found at {local_file_path}")
 
-      except PermissionError:
-         logger.error(f'No permision to read file {local_file_path}')
+        except PermissionError:
+            logger.error(f"No permision to read file {local_file_path}")
 
-      except Exception as e:
-         logger.error(f'Unexpected error while processing {blob.name}: {e}')
-         raise
+        except Exception as e:
+            logger.error(f"Unexpected error while processing {blob.name}: {e}")
+            raise
+
 
 def extract_and_load_bronze(local_dir):
-   os.makedirs(local_dir, exist_ok=True)
-   api_key = os.getenv('API_KEY')
-   headers = {'x-cg-demo-api-key' : api_key}
-   get_coins_market(headers, local_dir)
-   load_raw_data(local_dir)
+    os.makedirs(local_dir, exist_ok=True)
+    api_key = os.getenv("API_KEY")
+    headers = {"x-cg-demo-api-key": api_key}
+    get_coins_market(headers, local_dir)
+    load_raw_data(local_dir)
 
-if __name__ == '__main__':
-   extract_and_load_bronze('data/bronze')
+
+if __name__ == "__main__":
+    extract_and_load_bronze("data/bronze")
--- transform.py
+++ transform.py
@@ -4,49 +4,53 @@ import os
 import datetime as dt
 from datetime import date
 
+
 def transform_data(bronze_path, silver_dir) -> str:
     if not os.path.exists(bronze_path):
-        raise FileNotFoundError(f'File not found: {bronze_path}')
-    
-    with open(bronze_path, mode='r') as file:
+        raise FileNotFoundError(f"File not found: {bronze_path}")
+
+    with open(bronze_path, mode="r") as file:
         coins = json.load(fp=file)
 
-    df = pd.json_normalize(coins, sep='_')
+    df = pd.json_normalize(coins, sep="_")
 
-    df = df.rename(columns={
-        "id": "coin_id",
-        "image": "image_url",
-        "current_price": "current_price_usd",
-        "market_cap": "market_cap_usd",
-        "fully_diluted_valuation": "fully_diluted_valuation_usd",
-        "total_volume": "total_volume_usd",
-        "high_24h": "high_24h_usd",
-        "low_24h": "low_24h_usd",
-        "price_change_24h": "price_change_24h_usd",
-        "price_change_percentage_24h": "price_change_24h_pct",
-        "market_cap_change_24h": "market_cap_change_24h_usd",
-        "market_cap_change_percentage_24h": "market_cap_change_24h_pct",
-        "circulating_supply": "circulating_supply",
-        "total_supply": "total_supply",
-        "max_supply": "max_supply",
-        "ath": "all_time_high_usd",
-        "ath_change_percentage": "all_time_high_change_pct",
-        "ath_date": "all_time_high_date",
-        "atl": "all_time_low_usd",
-        "atl_change_percentage": "all_time_low_change_pct",
-        "atl_date": "all_time_low_date",
-        "roi_times": "return_on_investment_times",
-        "roi_currency": "return_on_investment_currency",
-        "roi_percentage": "return_on_investment_pct",
-        "last_updated": "last_updated_at"
-    })
+    df = df.rename(
+        columns={
+            "id": "coin_id",
+            "image": "image_url",
+            "current_price": "current_price_usd",
+            "market_cap": "market_cap_usd",
+            "fully_diluted_valuation": "fully_diluted_valuation_usd",
+            "total_volume": "total_volume_usd",
+            "high_24h": "high_24h_usd",
+            "low_24h": "low_24h_usd",
+            "price_change_24h": "price_change_24h_usd",
+            "price_change_percentage_24h": "price_change_24h_pct",
+            "market_cap_change_24h": "market_cap_change_24h_usd",
+            "market_cap_change_percentage_24h": "market_cap_change_24h_pct",
+            "circulating_supply": "circulating_supply",
+            "total_supply": "total_supply",
+            "max_supply": "max_supply",
+            "ath": "all_time_high_usd",
+            "ath_change_percentage": "all_time_high_change_pct",
+            "ath_date": "all_time_high_date",
+            "atl": "all_time_low_usd",
+            "atl_change_percentage": "all_time_low_change_pct",
+            "atl_date": "all_time_low_date",
+            "roi_times": "return_on_investment_times",
+            "roi_currency": "return_on_investment_currency",
+            "roi_percentage": "return_on_investment_pct",
+            "last_updated": "last_updated_at",
+        }
+    )
 
-    df['ingested_at'] = dt.datetime.now(tz=dt.timezone.utc)
-    df['snapshot_date'] = date.today()
+    df["ingested_at"] = dt.datetime.now(tz=dt.timezone.utc)
+    df["snapshot_date"] = date.today()
 
-    df.to_parquet(silver_dir, partition_cols=['snapshot_date'], engine='pyarrow')
+    df.to_parquet(silver_dir, partition_cols=["snapshot_date"], engine="pyarrow")
 
     return silver_dir
 
-if __name__ == '__main__': 
-    transform_data('data/bronze/coins_market.json', 'data/silver/coins_market/')
+
+if __name__ == "__main__":
+    transform_data("data/bronze/coins_market.json", "data/silver/coins_market/")
