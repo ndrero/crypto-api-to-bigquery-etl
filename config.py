9bb7a2c7be3ae0d789ec2ec38fe98752265303d2
ndrero <andreicarvs@gmail.com>
2026-02-11 23:03:16 -0300

    feat: add function to get big query auth and refact old credential function logic

--- config.py
+++ config.py
@@ -1,17 +1,23 @@
 import os
 from dotenv import load_dotenv
 from google.cloud import storage
+from google.cloud import bigquery
 from google.oauth2 import service_account
 
 load_dotenv()
 
-project_id = os.environ['GCP_PROJECT_ID']
-credentials_path = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+project_id = os.environ["GCP_PROJECT_ID"]
+credentials_path = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 
-def get_gcp_auth():
-   credentials = service_account.Credentials.from_service_account_file(credentials_path)
-   return storage.Client(project=project_id,credentials=credentials)
+
+def get_credentials():
+    return service_account.Credentials.from_service_account_file(credentials_path)
+
 
 def get_bucket(bucket_name):
-   client = get_gcp_auth()
-   return client.bucket(bucket_name)
+    client = storage.Client(project=project_id, credentials=get_credentials())
+    return client.bucket(bucket_name)
+
+
+def get_bq_client():
+    return bigquery.Client(project=project_id, credentials=get_credentials())
