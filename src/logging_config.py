6153d1dde817d4b21d90cc0d404940a682675adf
Andrei Carvalho <andreicarvs@gmail.com>
2026-02-16 13:40:29 -0300

    feat: set StreamHandler level to INFO

--- src/logging_config.py
+++ src/logging_config.py
@@ -2,28 +2,31 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
+
 def get_logger(name):
-   logger = logging.getLogger(name)
+    logger = logging.getLogger(name)
+    logger.setLevel(level=logging.DEBUG)
 
-   if not logger.handlers:
-      log_dir = 'logs'
-      if not os.path.exists(log_dir):
-         os.makedirs(log_dir)
+    if not logger.handlers:
+        log_dir = "logs"
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
 
-      logger.setLevel(level=logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(level=logging.INFO)
 
-      ch = logging.StreamHandler()
-      fh = RotatingFileHandler(filename='logs/job.log',
-                               maxBytes=10000,
-                               backupCount=3,
-                               mode='a')
+        fh = RotatingFileHandler(
+            filename="logs/job.log", maxBytes=10000, backupCount=3, mode="a"
+        )
 
-      formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
 
-      ch.setFormatter(formatter)
-      fh.setFormatter(formatter)
-      
-      logger.addHandler(fh)
-      logger.addHandler(ch)
+        ch.setFormatter(formatter)
+        fh.setFormatter(formatter)
 
-   return logger
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+
+    return logger
