2d3e5d2a02efe8e6ff37220121a0434574c94f20
Andrei Carvalho <andreicarvs@gmail.com>
2026-02-17 12:18:01 -0300

    refactor: modify logic to finding missing columns using sets, add logging and try exception

--- src/transform.py
+++ src/transform.py
@@ -2,54 +2,95 @@ import pandas as pd
 import json
 import datetime as dt
 from gcp_utils import get_bucket
+from logging_config import get_logger
+from config import BUCKET_NAME
+
+logger = get_logger(__name__)
 
 
 def process_bronze_to_silver(target_date, file_name) -> str:
-    bucket = get_bucket("crypto-prj-bucket")
-    blob = bucket.get_blob(f"bronze/{target_date}/{file_name}.json")
-    data = blob.download_as_text()
-    json_data = json.loads(data)
-    df = pd.json_normalize(json_data, sep="_")
+    try:
+        logger.info(f"Starting Bronze to Silver process for date: {target_date}")
+        bucket = get_bucket(BUCKET_NAME)
 
-    mapping_columns = {
-        "id": "coin_id",
-        "current_price": "current_price_usd",
-        "market_cap": "market_cap_usd",
-        "fully_diluted_valuation": "fully_diluted_valuation_usd",
-        "total_volume": "total_volume_usd",
-        "high_24h": "high_24h_usd",
-        "low_24h": "low_24h_usd",
-        "price_change_24h": "price_change_24h_usd",
-        "price_change_percentage_24h": "price_change_24h_pct",
-        "market_cap_change_24h": "market_cap_change_24h_usd",
-        "market_cap_change_percentage_24h": "market_cap_change_24h_pct",
-        "circulating_supply": "circulating_supply",
-        "total_supply": "total_supply",
-        "max_supply": "max_supply",
-        "ath": "all_time_high_usd",
-        "ath_change_percentage": "all_time_high_change_pct",
-        "ath_date": "all_time_high_date",
-        "atl": "all_time_low_usd",
-        "atl_change_percentage": "all_time_low_change_pct",
-        "atl_date": "all_time_low_date",
-        "roi_times": "return_on_investment_times",
-        "roi_currency": "return_on_investment_currency",
-        "roi_percentage": "return_on_investment_pct",
-        "last_updated": "last_updated_at",
-    }
+        blob_path = f"bronze/{target_date}/{file_name}.json"
 
-    df = df[list(mapping_columns.keys())].rename(columns=mapping_columns)
+        logger.debug(f"Reading file from: {blob_path}")
+        blob = bucket.get_blob(blob_path)
 
-    df["ingested_at"] = dt.datetime.now(tz=dt.timezone.utc)
+        if not blob:
+            logger.error(f"File not found at bucket path : {blob_path}")
+            raise FileNotFoundError
 
-    df["reference_dt"] = target_date
+        data = blob.download_as_text()
+        logger.info(f"Successfully download data for {target_date}")
 
-    df.to_parquet(
-        "gcs://crypto-prj-bucket/silver/crypto_market/",
-        partition_cols=["reference_dt"],
-        engine="pyarrow",
-        index=False,
-    )
+        json_data = json.loads(data)
+        df = pd.json_normalize(json_data, sep="_")
+
+        logger.info(f"Dataframe with {df.shape[0]} rows and {df.shape[1]} columns")
+        mapping_columns = {
+            "id": "coin_id",
+            "current_price": "current_price_usd",
+            "market_cap": "market_cap_usd",
+            "fully_diluted_valuation": "fully_diluted_valuation_usd",
+            "total_volume": "total_volume_usd",
+            "high_24h": "high_24h_usd",
+            "low_24h": "low_24h_usd",
+            "price_change_24h": "price_change_24h_usd",
+            "price_change_percentage_24h": "price_change_24h_pct",
+            "market_cap_change_24h": "market_cap_change_24h_usd",
+            "market_cap_change_percentage_24h": "market_cap_change_24h_pct",
+            "circulating_supply": "circulating_supply",
+            "total_supply": "total_supply",
+            "max_supply": "max_supply",
+            "ath": "all_time_high_usd",
+            "ath_change_percentage": "all_time_high_change_pct",
+            "ath_date": "all_time_high_date",
+            "atl": "all_time_low_usd",
+            "atl_change_percentage": "all_time_low_change_pct",
+            "atl_date": "all_time_low_date",
+            "roi_times": "return_on_investment_times",
+            "roi_currency": "return_on_investment_currency",
+            "roi_percentage": "return_on_investment_pct",
+            "last_updated": "last_updated_at",
+        }
+
+        available_columns = [col for col in mapping_columns.keys() if col in df.columns]
+
+        missing_columns = set(mapping_columns.keys()) - set(available_columns)
+
+        if missing_columns:
+            logger.info(f"Missing columns at response: {missing_columns}")
+
+        df = df[available_columns].rename(columns=mapping_columns)
+
+        initial_rows = df.shape[0]
+        df = df.drop_duplicates()
+        if df.shape[0] < initial_rows:
+            logger.info(f"Dropped {initial_rows - df.shape[0]} duplicate rows")
+
+        df["ingested_at"] = dt.datetime.now(tz=dt.timezone.utc)
+
+        df["reference_dt"] = target_date
+
+        logger.info(f"Dataframe transformation conclude. Final shape: {df.shape}")
+
+        output_path = f"gcs://{BUCKET_NAME}/silver/crypto_market/"
+        logger.info(f"Loading as parquet to {output_path}")
+        df.to_parquet(
+            output_path,
+            partition_cols=["reference_dt"],
+            engine="pyarrow",
+            index=False,
+        )
+        logger.info("Silver layer load complete.")
+
+    except Exception as e:
+        logger.error(
+            f"Critical failure processing {target_date}: {str(e)}", exc_info=True
+        )
+        raise e
 
 
 if __name__ == "__main__":
